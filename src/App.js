589ba15ab77a9ac5d513cbac3af245962c2d4fac
Swathi Govindarajalu <swathi_govindarajalu@trimble.com>
2023-03-17 07:28:34 -0400

    Created ToDoList Component

--- src/App.js
+++ src/App.js
@@ -1,37 +1,11 @@
 import React from 'react';
-
-
-var todoList = [
-  {
-    id: 1,
-    title: "Start Assignment"
-  },
-  {
-    id: 2,
-    title: "Assignment In Progress"
-  },
-
-  {
-    id: 3,
-    title: "Complete Assignment"
-  }];
-
-function ShowList() {
-  return todoList.map(i => {
-    return (<li key={i.id}>{i.title}</li>)
-  })
-}
-
-
-
+import TodoList from './TodoList';
 
 function App() {
   return (
     <div>
       <h1>Todo List</h1>
-      <ul>
-        {ShowList()}
-       </ul>
+       <TodoList/>
     </div>
   );
 }
