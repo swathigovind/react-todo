a7b99f1e63a84d24e04ee96f65d67d9370433b48
Swathi Govindarajalu <swathi_govindarajalu@trimble.com>
2023-03-20 23:15:03 -0400

    Lesson 1_2 - Changed to const

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,32 +1,33 @@
-import React from 'react';
+import React from "react";
 
 var todoList = [
-    {
-      id: 1,
-      title: "Start Assignment"
-    },
-    {
-      id: 2,
-      title: "Assignment In Progress"
-    },
-  
-    {
-      id: 3,
-      title: "Complete Assignment"
-    }];
+  {
+    id: 1,
+    title: "Start Assignment",
+  },
+  {
+    id: 2,
+    title: "Assignment In Progress",
+  },
 
-    
+  {
+    id: 3,
+    title: "Complete Assignment",
+  },
+];
 
-  let RenderList =  ()  => {
-    return todoList.map(i => {
-      return (<li key={i.id}>{i.title}</li>)
-    })
-  }
+const RenderList = () => {
+  return todoList.map((i) => {
+    return <li key={i.id}>{i.title}</li>;
+  });
+};
 
-let TodoList = () => {
-   return <ul>
-    <RenderList/>
-  </ul>
-}
+const TodoList = () => {
+  return (
+    <ul>
+      <RenderList />
+    </ul>
+  );
+};
 
 export default TodoList;
