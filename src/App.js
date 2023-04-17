703cbe92bbb1065060c476cfadc622f9f51ca196
Swathi Govindarajalu <swathi_govindarajalu@trimble.com>
2023-04-16 23:44:34 -0400

    Save Todo List in Storage -Formatted

--- src/App.js
+++ src/App.js
@@ -1,38 +1,33 @@
-import React from 'react';
-import TodoList from './TodoList';
-import AddTodoForm from './AddTodoForm';
+import React from "react";
+import TodoList from "./TodoList";
+import AddTodoForm from "./AddTodoForm";
 
 function App() {
 
-  const [todoList, setTodoList ] = React.useState(
-    JSON.parse(localStorage.getItem('savedTodoList')) || 'React'
-    );
-
+  
+  const [todoList, setTodoList] = React.useState(
+    JSON.parse(localStorage.getItem("savedTodoList")) || [
+      { title: "React", id: Date.now() },
+    ]
+  );
 
   React.useEffect(() => {
-    localStorage.setItem('savedTodoList', JSON.stringify(todoList) );
-    }, [todoList]);
-    
+    localStorage.setItem("savedTodoList", JSON.stringify(todoList));
+  }, [todoList]);
 
-  const addTodo  = (newTodo ) => {
 
-    setTodoList([...todoList,newTodo]);
-
+  const addTodo = (newTodo) => {
+    setTodoList([...todoList, newTodo]);
   };
 
   return (
     <div>
       <h1>Todo List</h1>
-      <AddTodoForm onAddTodo={addTodo}/>
-      
-      <TodoList todoList={todoList}/>     
+      <AddTodoForm onAddTodo={addTodo} />
+
+      <TodoList todoList={todoList} />
     </div>
   );
 }
-
-
-
-
-
 
 export default App;
