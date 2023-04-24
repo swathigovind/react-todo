a0fa718589a093937c6ce619515250e3e4e95351
Swathi Govindarajalu <swathi_govindarajalu@trimble.com>
2023-04-24 00:06:09 -0400

    Add Auto-Focus to Input

--- src/AddTodoForm.js
+++ src/AddTodoForm.js
@@ -7,6 +7,7 @@ import InputWithLabel from './InputWithLabel';
 const AddTodoForm = ({onAddTodo}) => {
 
   const [todoTitle , setTodoTitle  ] = React.useState('');
+  const [inputKey, setInputKey] = React.useState(0);
 
   const handleTitleChange = (event) => {
     const form = event.currentTarget;
@@ -26,12 +27,13 @@ const AddTodoForm = ({onAddTodo}) => {
       console.log(todoTitle);  
       onAddTodo({title: todoTitle, id:Date.now()});
       setTodoTitle('');
+      setInputKey((prevKey) => prevKey + 1); 
     }
   };
 
   return (
     <form id="add-todo-form" onSubmit={handleAddTodo}>
-      <InputWithLabel value={todoTitle} id="todoTitle" name="title"  onInputChange={handleTitleChange}>    
+      <InputWithLabel value={todoTitle} id="todoTitle" name="title"  isFocused onInputChange={handleTitleChange} key={inputKey} >    
       <strong>Title:</strong> 
       </InputWithLabel>    
       <button type="submit"> Add </button>
--- src/InputWithLabel.js
+++ src/InputWithLabel.js
@@ -1,15 +1,33 @@
 import React from "react";
 
-const InputWithLabel = ({ id, name, value, onInputChange, children  }) => {
+const InputWithLabel = ({
+  id,
+  name,
+  value,
+  onInputChange,
+  isFocused,
+  children,
+  key
+
+}) => {
+  const inputRef = React.useRef();
+
+  React.useEffect(() => {
+    if (isFocused && inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [isFocused]);
+
   return (
     <>
       <label htmlFor={id}>{children} </label>
       <input
+        ref={inputRef}
         name={name}
         id={id}
         value={value}
         onChange={onInputChange}
-        style={{ marginRight: "4px" }}
+        style={{ marginRight: "4px" }} key={key}
       ></input>
     </>
   );
