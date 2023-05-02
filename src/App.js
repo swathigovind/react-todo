57051f50424f9a43f65784059e3e3b4f0fae5fb9
SwathiGovindarajalu <swathi_govindarajalu@trimble.com>
2023-05-01 23:24:02 -0400

    Used Reducer.

--- src/App.js
+++ src/App.js
@@ -2,69 +2,78 @@ import React from "react";
 import TodoList from "./TodoList";
 import AddTodoForm from "./AddTodoForm";
 
-
-
-
 function App() {
-  const [todoList, setTodoList] = React.useState([]);
-
   const [isLoading, setIsLoading] = React.useState(true);
 
+  const todoListReducer = (state, action) => {
+    if (action.type === "SET_TODOLIST") {
+      return action.payload;
+    } else if (action.type === "REMOVE_TODOLIST") {
+      return state.filter((item) => action.payload !== item.id);
+    } else {
+      throw new Error();
+    }
+  };
 
+  const [todoList, dispatchTodoList] = React.useReducer(todoListReducer, []);
 
-  React.useEffect(() => {
-    const fetchData = () => {
-      return new Promise((resolve, reject) => {
-        setTimeout(() => {
+  const getToDoList = () =>
+    new Promise((resolve, reject) =>
+      setTimeout(
+        () =>
           resolve({
             data: {
-              todoList:  JSON.parse(localStorage.getItem("savedTodoList")) || [
+              todoList: JSON.parse(localStorage.getItem("savedTodoList")) || [
                 { title: "React", id: Date.now() },
-              ]
-            }
-          });
-        }, 2000);
-      });
-    };
+              ],
+            },
+          }),
+        2000
+      )
+    );
 
-    fetchData().then(result => {
-      setTodoList(result.data.todoList);
+  React.useEffect(() => {
+    getToDoList().then((result) => {
+      dispatchTodoList({
+        type: "SET_TODOLIST",
+        payload: result.data.todoList,
+      });
+
       setIsLoading(false);
     });
   }, [todoList]);
 
-
-
   React.useEffect(() => {
     if (!isLoading) {
-    localStorage.setItem("savedTodoList", JSON.stringify(todoList));
+      localStorage.setItem("savedTodoList", JSON.stringify(todoList));
     }
   }, [todoList, isLoading]);
 
-
-
   const addTodo = (newTodo) => {
-    setTodoList([...todoList, newTodo]);
+    dispatchTodoList({
+      type: "SET_TODOLIST",
+      payload: [...todoList, newTodo],
+    });
   };
 
-
-
   const removeTodo = (id) => {
-    const newTodoList = todoList.filter(
-      (item) => id !== item.id
-    );
-    setTodoList(newTodoList);
+    dispatchTodoList({
+      type: "REMOVE_TODOLIST",
+      payload: id,
+    });
   };
 
   return (
     <>
       <h1>Todo List</h1>
       <AddTodoForm onAddTodo={addTodo} />
-       {isLoading ? (<p > Loading...</p>) :
-      <TodoList todoList={todoList} onRemoveTodo={removeTodo} /> }
+      {isLoading ? (
+        <p> Loading...</p>
+      ) : (
+        <TodoList todoList={todoList} onRemoveTodo={removeTodo} />
+      )}
     </>
   );
 }
-
 
 export default App;
