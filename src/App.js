b09b3a2d3fa59925d936e7c0adad6f7fc11dd531
SwathiGovindarajalu <swathi_govindarajalu@trimble.com>
2023-05-01 21:58:07 -0400

    Add Loading State

--- src/App.js
+++ src/App.js
@@ -8,13 +8,19 @@ import AddTodoForm from "./AddTodoForm";
 function App() {
   const [todoList, setTodoList] = React.useState([]);
 
+  const [isLoading, setIsLoading] = React.useState(true);
+
+
+
   React.useEffect(() => {
     const fetchData = () => {
       return new Promise((resolve, reject) => {
         setTimeout(() => {
           resolve({
             data: {
-              todoList: todoList // Use the initial/default list state
+              todoList:  JSON.parse(localStorage.getItem("savedTodoList")) || [
+                { title: "React", id: Date.now() },
+              ]
             }
           });
         }, 2000);
@@ -23,16 +29,25 @@ function App() {
 
     fetchData().then(result => {
       setTodoList(result.data.todoList);
+      setIsLoading(false);
     });
   }, [todoList]);
 
+
+
   React.useEffect(() => {
+    if (!isLoading) {
     localStorage.setItem("savedTodoList", JSON.stringify(todoList));
-  }, [todoList]);
+    }
+  }, [todoList, isLoading]);
+
+
 
   const addTodo = (newTodo) => {
     setTodoList([...todoList, newTodo]);
   };
+
+  
 
   const removeTodo = (id) => {
     const newTodoList = todoList.filter(
