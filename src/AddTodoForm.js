0741711d4be15b1d28998a4940bf29fa50f62151
SwathiGovindarajalu <swathi_govindarajalu@trimble.com>
2023-05-15 12:06:11 -0400

    Refactor based on reducer pattern

--- src/AddTodoForm.js
+++ src/AddTodoForm.js
@@ -1,41 +1,66 @@
-import React from 'react';
-import InputWithLabel from './InputWithLabel';
+import React from "react";
+import InputWithLabel from "./InputWithLabel";
 
+const inputReducer = (state, action) => {
+  switch (action.type) {
+    case "SET_INPUT":
+      return {
+        ...state,
+        todoTitle: action.payload,
+      };
+    case "RESET_INPUT":
+      return {
+        ...state,
+        todoTitle: action.payload,
+        inputKey: state.inputKey + 1,
+      };
+    default:
+      throw new Error();
+  }
+};
 
+const AddTodoForm = ({ onAddTodo }) => {
 
-
-const AddTodoForm = ({onAddTodo}) => {
-
-  const [todoTitle , setTodoTitle  ] = React.useState('');
-  const [inputKey, setInputKey] = React.useState(0);
+  const [state, dispatch] = React.useReducer(inputReducer, {
+    todoTitle: "",
+    inputKey: 0,
+  });
 
   const handleTitleChange = (event) => {
     const form = event.currentTarget;
     if (form) {
-     let newTodoTitle = form.value;
-     setTodoTitle(newTodoTitle);
+      let newTodoTitle = form.value;
+      dispatch({
+        type: "SET_INPUT",
+        payload: newTodoTitle,
+      });
     }
- 
   };
 
-
   const handleAddTodo = (event) => {
-
     event.preventDefault();
     const form = event.currentTarget;
-    if (form) {   
-      console.log(todoTitle);  
-      onAddTodo({title: todoTitle, id:Date.now()});
-      setTodoTitle('');
-      setInputKey((prevKey) => prevKey + 1); 
+    if (form) {
+      onAddTodo({ title: state.todoTitle, id: Date.now() });
+      dispatch({
+        type: "RESET_INPUT",
+        payload: "",
+      });
     }
   };
 
   return (
     <form id="add-todo-form" onSubmit={handleAddTodo}>
-      <InputWithLabel value={todoTitle} id="todoTitle" name="title"  isFocused onInputChange={handleTitleChange} key={inputKey} >    
-      <strong>Title:</strong> 
-      </InputWithLabel>    
+      <InputWithLabel
+        value={state.todoTitle}
+        id="todoTitle"
+        name="title"
+        isFocused
+        onInputChange={handleTitleChange}
+        key={state.inputKey}
+      >
+        <strong>Title:</strong>
+      </InputWithLabel>
       <button type="submit"> Add </button>
     </form>
   );
